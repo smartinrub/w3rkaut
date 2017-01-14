e42087e639f1cac8e12fdad2407519b6bfa612c4
sergio <econsergio@gmail.com>
2017-01-14 20:57:18 +0100

    RecyclerViewFragment modified to implement update option

--- app/src/main/java/net/dynu/w3rkaut/presentation/ui/fragments/RecyclerViewFragment.java
+++ app/src/main/java/net/dynu/w3rkaut/presentation/ui/fragments/RecyclerViewFragment.java
@@ -8,8 +8,10 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -18,20 +20,15 @@ import net.dynu.w3rkaut.domain.executor.impl.ThreadExecutor;
 import net.dynu.w3rkaut.presentation.Model.Location;
 import net.dynu.w3rkaut.presentation.presenters.LocationListPresenter;
 import net.dynu.w3rkaut.presentation.presenters.impl.LocationListPresenterImpl;
-import net.dynu.w3rkaut.presentation.ui.activities.MainActivity;
 import net.dynu.w3rkaut.presentation.ui.adapters.RecyclerBindingAdapter;
 import net.dynu.w3rkaut.storage.LocationRepositoryImpl;
 import net.dynu.w3rkaut.threading.MainThreadImpl;
-import net.dynu.w3rkaut.utils.CurrentTime;
 import net.dynu.w3rkaut.utils.LocationHandler;
 
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import timber.log.Timber;
 
 public class RecyclerViewFragment extends Fragment implements
         LocationListPresenter.View, RecyclerBindingAdapter.OnItemClickListener, RecyclerBindingAdapter.OnItemLongClickListener {
@@ -46,15 +43,15 @@ public class RecyclerViewFragment extends Fragment implements
 
     private LocationListPresenterImpl presenter;
 
-    private Timer timer;
+    private Timer listTimer;
+    private Timer latLngTimer;
     private ProgressDialog progressDialog;
 
-    private Double latitude;
-    private Double longitude;
+    private Double currLatitude;
+    private Double currLongitude;
 
 
     private List<Location> locations;
-
 
     private static final Comparator<Location> DISTANCE_COMPARATOR =
             new Comparator<Location>() {
@@ -83,17 +80,19 @@ public class RecyclerViewFragment extends Fragment implements
         rootView = inflater.inflate(R.layout.fragment_recycler_view_locations,
                 container, false);
 
-        init();
+        getCurrentLocation();
+
 
         return rootView;
     }
 
     private void init() {
+
         presenter = new LocationListPresenterImpl
                 (ThreadExecutor.getInstance(), MainThreadImpl.getInstance(),
                         this,
                         new LocationRepositoryImpl(getActivity()), new LatLng
-                        (0.0, 0.0));
+                        (currLatitude, currLongitude));
         presenter.getAllLocations();
 
         setupRecyclerView();
@@ -102,7 +101,24 @@ public class RecyclerViewFragment extends Fragment implements
     private void setupRecyclerView() {
         recyclerView = (RecyclerView) rootView.findViewById(R.id
                 .recycler_view);
+        swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id
+                .swipe_refresh_layout);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        });
     }
 
     @Override
@@ -139,8 +155,8 @@ public class RecyclerViewFragment extends Fragment implements
     public void showLocations(List<Location> locations) {
         this.locations = locations;
         showProgress();
-        timer = new Timer();
-        timer.schedule(new GetAllLocationsTask(), 200, 200);
+        listTimer = new Timer();
+        listTimer.schedule(new GetAllLocationsTask(), 200, 50);
     }
 
     @Override
@@ -153,14 +169,64 @@ public class RecyclerViewFragment extends Fragment implements
 
     }
 
-    class GetAllLocationsTask extends TimerTask implements RecyclerBindingAdapter.OnItemClickListener, RecyclerBindingAdapter.OnItemLongClickListener {
+
+    public void getCurrentLocation() {
+        locationHandler = new LocationHandler(getActivity());
+        latLngTimer = new Timer();
+        latLngTimer.schedule(new GetCurrentLocationTask(), 0, 50);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                presenter.getAllLocations();
+                recyclerBindingAdapter.replaceAll(locations);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    class GetCurrentLocationTask extends TimerTask {
+        @Override
+        public void run() {
+            Double latitude = locationHandler.getLatitude();
+            Double longitude = locationHandler.getLongitude();
+            if (latitude != null && longitude != null) {
+                currLatitude = latitude;
+                currLongitude = longitude;
+                latLngTimer.cancel();
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        init();
+                    }
+                });
+            }
+        }
+    }
+
+
+    class GetAllLocationsTask extends TimerTask implements
+            RecyclerBindingAdapter.OnItemClickListener,
+            RecyclerBindingAdapter.OnItemLongClickListener {
         @Override
         public void run() {
             if(locations != null) {
                 recyclerBindingAdapter = new RecyclerBindingAdapter(getContext(),
                         DISTANCE_COMPARATOR, this, this);
                 recyclerBindingAdapter.add(locations);
-                timer.cancel();
+                swipeRefreshLayout.setOnRefreshListener(
+                        new SwipeRefreshLayout.OnRefreshListener() {
+                            @Override
+                            public void onRefresh() {
+                                presenter.getAllLocations();
+                                recyclerBindingAdapter.replaceAll(locations);
+                                swipeRefreshLayout.setRefreshing(false);
+                            }
+                        }
+                );
+                listTimer.cancel();
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -173,7 +239,8 @@ public class RecyclerViewFragment extends Fragment implements
 
         @Override
         public void onItemClick(Location item) {
-
+            Toast.makeText(getActivity(), item.getUserFirstName(),
+                    Toast.LENGTH_SHORT).show();
         }
 
         @Override
