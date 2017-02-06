200496b6e036ea5c35b2e3aaffeb74b47da40b16
sergio <econsergio@gmail.com>
2017-02-06 21:37:02 +0100

    volly with get all locations

---
+++ app/src/main/java/net/dynu/w3rkaut/network/VolleySingleton.java
@@ -0,0 +1,68 @@
+package net.dynu.w3rkaut.network;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * This class implements pattern in order to create only one Volley instance
+ *
+ * @see <a href="https://developer.android.com/training/volley/requestqueue.html">Android
+ * Developers Documentation</a>
+ */
+public class VolleySingleton {
+    private static VolleySingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
+
+    private VolleySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
--- app/src/main/java/net/dynu/w3rkaut/storage/LocationRepositoryImpl.java
+++ app/src/main/java/net/dynu/w3rkaut/storage/LocationRepositoryImpl.java
@@ -2,11 +2,24 @@ package net.dynu.w3rkaut.storage;
 
 import android.content.Context;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import net.dynu.w3rkaut.domain.respository.LocationRepository;
 import net.dynu.w3rkaut.network.RestClient;
+import net.dynu.w3rkaut.network.VolleySingleton;
 import net.dynu.w3rkaut.network.services.SyncService;
 import net.dynu.w3rkaut.network.converters.RESTLocationConverter;
 import net.dynu.w3rkaut.network.model.RESTLocation;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,6 +27,9 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Response;
+import timber.log.Timber;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 /**
@@ -21,10 +37,14 @@ import retrofit2.Response;
  *
  * @author Sergio Martin Rubio
  */
-public class LocationRepositoryImpl implements LocationRepository {
+public class LocationRepositoryImpl implements LocationRepository, com.android.volley.Response.ErrorListener, com.android.volley.Response.Listener<String> {
+
+    public static final String REST_API_URL = "https://w3rkaut.dynu" +
+            ".net/android/php/";
 
     private Context context;
     private String message = "";
+    private List<RESTLocation> locations;
 
     public LocationRepositoryImpl(Context context) {
         this.context = context;
@@ -55,19 +75,29 @@ public class LocationRepositoryImpl implements LocationRepository {
         return message;
     }
 
+//    @Override
+//    public List<RESTLocation> getAll() {
+//        List<RESTLocation> locations = new ArrayList<>();
+//        SyncService syncService = RestClient.getApiService();
+//        Call<List<RESTLocation>> call = syncService.getAllLocations();
+//
+//        try {
+//            Response<List<RESTLocation>> response = call.execute();
+//            locations = response.body();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return locations;
+//    }
+
     @Override
     public List<RESTLocation> getAll() {
-        List<RESTLocation> locations = new ArrayList<>();
-        SyncService syncService = RestClient.getApiService();
-        Call<List<RESTLocation>> call = syncService.getAllLocations();
-
-        try {
-            Response<List<RESTLocation>> response = call.execute();
-            locations = response.body();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        locations = new ArrayList<>();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                REST_API_URL + "locations.php", this, this);
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue
+                (stringRequest);
         return locations;
     }
 
@@ -85,5 +115,34 @@ public class LocationRepositoryImpl implements LocationRepository {
         }
 
         return message;
+    }
+
+    @Override
+    public void onResponse(String response) {
+        try {
+            JSONArray jsonArray = new JSONArray(response);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                long userId = jsonObject.getInt("user_id");
+                String firstName = jsonObject.getString("first_name");
+                String lastName = jsonObject.getString("last_name");
+                double lat = Double.parseDouble(jsonObject.getString("latitude"));
+                double lng = Double.parseDouble(jsonObject.getString("longitude"));
+                String timeRemaining = jsonObject.getString("time_remaining");
+                String postedAt = jsonObject.getString("posted_at");
+                RESTLocation location = new RESTLocation(userId,
+                        firstName, lastName, lat, lng, timeRemaining,
+                        postedAt);
+                locations.add(location);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Timber.e("error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
     }
 }
